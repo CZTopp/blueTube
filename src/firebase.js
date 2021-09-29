8be90d9056b10fd03bd70833f62c02f4ba58e3b6
CZTopp <czshows@gmail.com>
2021-09-29 17:25:26 -0400

    clean up commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAgc_ZnpkK4bhfLJZ1MiwzquIrfOYqROYg",
+  authDomain: "bluetube-mp.firebaseapp.com",
+  projectId: "bluetube-mp",
+  storageBucket: "bluetube-mp.appspot.com",
+  messagingSenderId: "1006994732570",
+  appId: "1:1006994732570:web:df02c31cb89deda2fc03a3",
+  measurementId: "G-B420CVP3H9",
+}
