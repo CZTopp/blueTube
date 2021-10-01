22f1469ac4273047f953f2e3a166223350c0c134
CZTopp <czshows@gmail.com>
2021-09-30 20:16:53 -0400

    add sidebar list

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,61 @@
 import React from "react"
-import { Grid } from "@material-ui/core"
+import { Grid, List, ListItem } from "@material-ui/core"
+import { Divider } from "@mui/material"
 import "./Sidebar.css"
 import SidebarRow from "../SidebarRow/SidebarRow"
 import HomeIcon from "@material-ui/icons/Home"
 import WhatshotIcon from "@material-ui/icons/Whatshot"
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions"
+import {
+  ExpandMoreOutlined,
+  OndemandVideo,
+  ThumbUpAltOutlined,
+  VideoLibrary,
+  History,
+  WatchLater,
+} from "@material-ui/icons"
 
 function Sidebar() {
   return (
     <>
-      <Grid container className="sidebar">
-        <SidebarRow Icon={HomeIcon} title="Home" />
-        <SidebarRow Icon={WhatshotIcon} title="Trending" />
-        <SidebarRow Icon={SubscriptionsIcon} title="Subscriptions" />
+      <Grid className="sidebar">
+        <List>
+          <ListItem>
+            <SidebarRow selected Icon={HomeIcon} title="Home" />
+          </ListItem>
+          <ListItem>
+            <SidebarRow Icon={WhatshotIcon} title="Trending" />
+          </ListItem>
+          <ListItem>
+            <SidebarRow Icon={SubscriptionsIcon} title="Subscriptions" />
+          </ListItem>
+        </List>
+        <Grid item>
+          <Divider orientation="horizontal" className="divider" />
+        </Grid>
+        <List>
+          <ListItem>
+            <SidebarRow Icon={VideoLibrary} title="Library" />
+          </ListItem>
+          <ListItem>
+            <SidebarRow Icon={History} title="History" />
+          </ListItem>
+          <ListItem>
+            <SidebarRow Icon={OndemandVideo} title="Your videos" />
+          </ListItem>
+          <ListItem>
+            <SidebarRow Icon={WatchLater} title="Watch Later" />
+          </ListItem>
+          <ListItem>
+            <SidebarRow Icon={ThumbUpAltOutlined} title="Liked videos" />
+          </ListItem>
+          <ListItem>
+            <SidebarRow Icon={ExpandMoreOutlined} title="Show more" />
+          </ListItem>
+        </List>
       </Grid>
+
+      {/* <hr /> */}
     </>
   )
 }
--- src/components/SidebarRow/SidebarRow.jsx
+++ src/components/SidebarRow/SidebarRow.jsx
@@ -3,14 +3,12 @@ import { Grid, Box } from "@material-ui/core"
 import Typography from "@mui/material/Typography"
 import "./SidebarRow.css"
 
-function SidebarRow({ Icon, title }) {
+function SidebarRow({ selected, Icon, title }) {
   return (
     <>
-      <Grid className="sidebarRow">
-        <Icon className="sidebarRow__icon" />
-        <Typography component="h2" className="sidebarRow__title">
-          <Box sx={{ fontWeight: "bold" }}>{title}</Box>
-        </Typography>
+      <Grid className={`sidebarRow ${selected && "selected"}`}>
+        <Icon className={"sidebarRow__icon"} />
+        <h2 className="sidebarRow__title">{title}</h2>
       </Grid>
     </>
   )
